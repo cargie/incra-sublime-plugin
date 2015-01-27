20ecc152d68967be328b9343987e0c92573ccc7a
cargie <cargiejayectin@gmail.com>
2015-01-27 15:21:54 +0800

    repair indentation

--- incracompletion.py
+++ incracompletion.py
@@ -7,20 +7,20 @@ import os
 
 class AllAutocomplete(sublime_plugin.EventListener):
 
-		def on_new(self,view):
-			print("carl")
+	def on_new(self,view):
+		print("carl")
 
-		def on_query_completions(self, view, prefix, locations):
-			for root, dirs, files in os.walk('/data/ShopData/Test/designs/Sample6/'):
-				for file in files:
-					if file.startswith(prefix):
-						matches = [(w, w.replace('.html', '')) for w in files]
-						print (words)
+	def on_query_completions(self, view, prefix, locations):
+		for root, dirs, files in os.walk('/data/ShopData/Test/designs/Sample6/'):
+			for file in files:
+				if file.startswith(prefix):
+					matches = [(w, w.replace('.html', '')) for w in files]
+					print (words)
 						
 
 if sublime.version() >= '3000':
-  def is_empty_match(match):
-    return match.empty()
+  	def is_empty_match(match):
+    	return match.empty()
 else:
-  def is_empty_match(match):
-    return match is None
+  	def is_empty_match(match):
+    	return match is None
