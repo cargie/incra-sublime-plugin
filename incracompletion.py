490e1a8e9afa819cdaef8114f89cd39c7763584b
cargie <cargiejayectin@gmail.com>
2015-02-16 15:44:10 +0800

    renew

--- incracompletion.py
+++ incracompletion.py
@@ -1,23 +1,84 @@
 import sublime_plugin
 import sublime
-import os
+import os, fnmatch,re
 
 # limits to prevent bogging down the system
-
-
-class AllAutocomplete(sublime_plugin.EventListener):
+result = []
+class IncraComplete(sublime_plugin.EventListener):
 
 	def on_query_completions(self, view, prefix, locations):
-		for root, dirs, files in os.walk('/data/ShopData/Test/designs/Sample6/'):
-			for file in files:
-				if file.startswith(prefix):
-					matches = [(w, w.replace('.html', '')) for w in files]
-					print (words)
-						
+		#print(view.word())
+		open_bracket = view.substr(sublime.Region(locations[0],locations[0]-2))
+		close_bracket = view.substr(sublime.Region(locations[0],locations[0]+1))
+		trigger = view.substr(sublime.Region(locations[0],locations[0]-1))
+		extension = os.path.splitext(view.file_name())[1]
+		print(open_bracket,close_bracket)
+		if (open_bracket == "[:" and extension == ".html" and close_bracket == "]") or (trigger == '?' and close_bracket == "]"):
+			print(trigger,extension)
+			try:
+				regex = re.compile(prefix, re.IGNORECASE)
+				result.sort()
+				matches = [string for string in result if re.search(regex, string)]
+				# print (matches)
+				match = [(w, w.replace('.html', '',)) for w in matches]
+				# print (match,prefix)
+				print(match)
+				return match
+			except ValueError:
+				print(e)
 
-if sublime.version() >= '3000':
-  	def is_empty_match(match):
-    	return match.empty()
-else:
-  	def is_empty_match(match):
-    	return match is None
+		return []
+
+	def without_duplicates(words):
+		result = []
+		for w in words:
+			if w not in result:
+				result.append(w)
+				return result
+
+	def on_new(self,view):
+		dirs = sublime.active_window().folders()
+		if len(dirs) == 1:
+			for d in dirs:
+				for root, dirs, files in os.walk(d):
+					for file in files:
+					# without_duplicates(files)
+					# print (files)
+						matches = [(w, w.replace('.html', '',)) for w in files]
+					# print (matches)
+						for m in files:
+							if m not in  result:
+								result.append(m)
+								print (result)
+
+
+	def on_post_save(self,view):
+		dirs = sublime.active_window().folders()
+		if len(dirs) == 1:
+			for d in dirs:
+				for root, dirs, files in os.walk(d):
+					for file in files:
+					# without_duplicates(files)
+					# print (files)
+						matches = [(w, w.replace('.html', '',)) for w in files]
+					# print (matches)
+						for m in files:
+							if m not in  result:
+								result.append(m)
+								print (result)
+
+
+	def __init__(self):
+		dirs = sublime.active_window().folders()
+		if len(dirs) == 1:
+			for d in dirs:
+				for root, dirs, files in os.walk(d):
+					for file in files:
+					# without_duplicates(files)
+					# print (files)
+						matches = [(w, w.replace('.html', '',)) for w in files]
+					# print (matches)
+						for m in files:
+							if m not in  result:
+								result.append(m)
+								# print (result)
