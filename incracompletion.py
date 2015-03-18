cd7bdf7716a1c63e316c94e4a4bb495da3c06e5b
cargie <cargiejayectin@gmail.com>
2015-03-18 17:11:31 +0800

    new

--- incracompletion.py
+++ incracompletion.py
@@ -1,30 +1,15 @@
 import sublime_plugin
 import sublime
 import os, fnmatch,re
-
+from . import replacers
 result = []
-def get_scope_attribute():
-	return ({
-		'Address':[],
-		'Admin':[],
-		'Attribute':[],
-		'AttributeGroup':[],
-		'AttributeOption':[],
-		'BaseProduct':[],
-		'Block':[],
-		'BookYear':[],
-		'Product':['Name','String'],
-		'Brand':['Id','Image'],
-		'Category':['Static','Blootic'],
-		}
-	)
 
+def get_scope_attribute():
+	return replacers.replacer_list
 # limits to prevent bogging down the system
 class IncraComplete(sublime_plugin.EventListener):
 
 	def on_query_completions(self, view, prefix, locations):
-
-		# print(prefix,len(prefix))
 		start= locations[0]
 		end = locations[0]
 		open_bracket_colon = view.substr(sublime.Region(locations[0],locations[0]-2))
@@ -38,26 +23,36 @@ class IncraComplete(sublime_plugin.EventListener):
 				regex = re.compile(prefix, re.IGNORECASE)
 				result.sort()
 				matches = [string for string in result if re.search(regex, string)]
-				match = [(w, (w.replace('.html', '',))+ ({True:'',False:']'}[close_bracket==']']) )  for w in matches]
+				match = [(w+'\t Block', (w.replace('.html', '',))+ ({True:'',False:']'}[close_bracket==']']) )  for w in matches]
 				
 				return (match,sublime.INHIBIT_WORD_COMPLETIONS | sublime.INHIBIT_EXPLICIT_COMPLETIONS)
 				
-
 			except ValueError:
 				print(e)
-		elif(open_bracket== "[" and extension == ".html"):
-			return [(w, (w.replace('.html', '',))+ ({True:'',False:'$0]'}[close_bracket==']']) )  for w in sorted(get_scope_attribute().keys())]
+		elif(open_bracket== "[" and extension == ".html" or trigger == '?'):
+			return ([(w+'\t Scope', (w.replace('.html', '',))+ ({True:'',False:'$0]'}[close_bracket==']']) )  for w in sorted(get_scope_attribute().keys())],sublime.INHIBIT_WORD_COMPLETIONS | sublime.INHIBIT_EXPLICIT_COMPLETIONS)
 
-		elif(close_bracket=="]"):
+		elif(trigger == '.' and (close_bracket=="]" or close_bracket != ']')):
+
 			vw = ''
-			x = 0
-			while(vw != '['):
+			x = 1
+			# while(vw != '['):
+			# 	vw = view.substr(sublime.Region(start-1-x, end-x))
+			# 	x=x+1
+			# 	if (x> 15):
+			# 		break
+			while True:
 				vw = view.substr(sublime.Region(start-1-x, end-x))
-				x=x+1
-				print(vw)
-			vw = view.substr(sublime.Region(start-x+1, end-1))
-			return [(w,w) for w in (get_scope_attribute().get(vw))]
-		return []
+				if (vw == '[' or vw =='?'):
+					vw = view.substr(sublime.Region(start-1-x, end-x))
+					print(vw)
+					break
+				else:
+					x+=1
+			print(x)
+			vw = view.substr(sublime.Region(start-x, end-1))
+			print (vw)
+			return ([(w+'\tAttribute',(w)+ ({True:'',False:'$0]'}[close_bracket==']'])) for w in (get_scope_attribute().get(vw))],sublime.INHIBIT_WORD_COMPLETIONS | sublime.INHIBIT_EXPLICIT_COMPLETIONS)
 
 	def on_post_save(self,view):
 		dirs = sublime.active_window().folders()
